60d2fe7b0f3dca9160179a1476d12505af058347
Sly <SlyBalto@Gmail.com>
2023-12-09 11:01:57 -0600

    test project structure

---
+++ src/main/java/CRRW/MyPlushie/MyPlushieApplication.java
@@ -0,0 +1,13 @@
+package CRRW.MyPlushie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPlushieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPlushieApplication.class, args);
+	}
+
+}
