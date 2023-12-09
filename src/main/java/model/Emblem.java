817b64b55f99bf682eacd037d056795d3e49f1f5
Sly <SlyBalto@Gmail.com>
2023-12-09 11:12:10 -0600

    test project structure

---
+++ src/main/java/model/Emblem.java
@@ -0,0 +1,24 @@
+package model;
+
+public enum Emblem {
+    NONE("None"),
+    HEART("Heart"), // pink
+    FLAME("Flame"), // red
+    SUN("Sun"), // orange
+    ZAP("Zap"),  // yellow
+    LEAF("Leaf"), // green
+    SPLASH("Splash"), // blue
+    DIAMOND("Diamond"), // purple
+    MOON("Moon"), // white
+    SKULL("Skull"); // black
+
+    private final String displayName;
+
+    Emblem(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
