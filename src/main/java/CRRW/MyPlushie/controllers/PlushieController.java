0f7ad9dd56c82f9d61b57f382c2fd2307b7400bd
DanaC314 <139182965+DanaC314@users.noreply.github.com>
2023-12-16 19:46:21 -0600

    working on plushie controller

--- src/main/java/CRRW/MyPlushie/controllers/PlushieController.java
+++ src/main/java/CRRW/MyPlushie/controllers/PlushieController.java
@@ -1,6 +1,40 @@
 package CRRW.MyPlushie.controllers;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import CRRW.MyPlushie.services.UserService;
+
+@Controller
+@RequestMapping("/plushies")
 public class PlushieController {
-    @GetMapping("/plushies")
-    public String getAllPlushies(Model model)
+
+    @Autowired
+    private PlushieService plushieService;
+
+    // Mapping for the list view
+    @GetMapping("/list")
+    public String listPlushies(Model model) {
+        List<Plushie> plushies = plushieService.getAllPlushies();
+        model.addAttribute("plushies", plushies);
+        return "plushie/list";
+    }
+
+    // Mapping for the form to add a new plushie
+    @GetMapping("/add")
+    public String showAddPlushieForm(Model model) {
+        model.addAttribute("plushie", new Plushie());
+        return "plushie/add";
+    }
+
+    // Mapping to process the form submission
+    @PostMapping("/add")
+    public String addPlushie(Plushie plushie) {
+        plushieService.addPlushie(plushie);
+        return "redirect:/plushies/list";
+    }
 }
+
